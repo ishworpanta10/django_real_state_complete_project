06a7a11296e3f8864d0ce9e6a1eaf7890effff81
ishworpanta10 <ishworpanta10@gmail.com>
2020-08-27 12:25:02 +0545

    contact enqiry done

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Contact
 
-admin.site.register(Contact)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'listing', 'email', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing')
+    list_per_page = 25
+
+
+admin.site.register(Contact, ContactAdmin)
